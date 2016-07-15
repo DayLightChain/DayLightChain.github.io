082ebe4efcb7b41b9dccb60849065423093873d7
Yuriy <yuriy.lomakin@gmail.com>
2016-07-15 13:25:03 +0300

    add send form

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,24 @@
 $(document).ready(function(){
 	'use strict';
 	
+	$("#send_email").bind('click', function () {
+		if ($("#email").val().indexOf('@')===-1) {
+			return false;
+		} else {
+			$.ajax({
+				url: 'http://148.251.83.120:8093/subscribe',
+				type: 'POST',
+				data: {
+					email: $("#email").val()
+				},
+				success: function(data){
+					$("#form").hide();
+					$("#message").html('Thank you!').show();
+				}    
+			});
+		}
+	});
+	
 	jQuery.os = { name: (/(win|mac|linux|sunos|solaris|iphone|ipad)/.exec(navigator.platform.toLowerCase()) || [u])[0].replace('sunos', 'solaris') };
 	if (jQuery.os.name === "mac" || jQuery.os.name === "iphone" || jQuery.os.name === "ipad") {
 		$("body").addClass("macfix");
